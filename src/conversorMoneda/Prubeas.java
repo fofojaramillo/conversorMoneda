e98456e9e6e2803f98ebbdec125b6b5472adc61e
Rodolfo <rodolfo.jaramillo@outlook.com>
2023-03-07 21:51:38 -0700

    Terminadas funcionalidades del conversor de monedass

---
+++ src/conversorMoneda/Prubeas.java
@@ -0,0 +1,16 @@
+package conversorMoneda;
+
+import javax.swing.JOptionPane;
+
+
+public class Prubeas {
+	public static void main(String[] args) {
+	
+	int resp;
+	
+	resp=JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+
+	System.out.println(resp);
+	
+	}
+}
